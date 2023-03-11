bc6416e7a86fab1f9415418971d5c970d0c69a9e
moritz loeser <moritz.loeser@prodyna.com>
2023-03-11 17:26:12 +0100

    add fileendpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result, HttpRequest};
+use actix_files::NamedFile;
+use std::path::PathBuf;
+
+async fn index(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -21,6 +28,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/{filename:.*}", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
